26e6f4a23e9aff29d7888083ff90a14f38914d39
DanHaziel <danhazielnm@gmail.com>
2025-03-06 13:41:29 +0100

    Adding new elements in order to handle personal datas from users

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,13 +1,25 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:quizz_game/components/components.dart';
 import 'package:quizz_game/services/auth_service.dart';
+import 'package:quizz_game/errors/error_utils.dart';
+import 'package:quizz_game/pages/home_page.dart';
 
 class RegisterPage extends StatefulWidget {
   final Function()? onTap;
+  final String firstName;
+  final String lastName;
+  final String residence;
+  final String newBirthDate;
+
   const RegisterPage({
     super.key,
-    required this.onTap
+    required this.onTap,
+    required this.firstName,
+    required this.lastName,
+    required this.residence,
+    required this.newBirthDate,
   });
 
   @override
@@ -36,15 +48,30 @@ class _RegisterPageState extends State<RegisterPage> {
       try{
         // check if the password is confirmed
         if (passwordController.text == confirmPasswordController.text) {
-          await FirebaseAuth.instance.createUserWithEmailAndPassword(
-            email: emailController.text, 
-            password: passwordController.text,
+          UserCredential userCredential =
+              await FirebaseAuth.instance.createUserWithEmailAndPassword(
+            email: emailController.text.trim(),
+            password: passwordController.text.trim(),
           );
+
+          await FirebaseFirestore.instance
+              .collection("users")
+              .doc(userCredential.user!.uid)
+              .set({
+            "firstName": widget.firstName,
+            "lastName": widget.lastName,
+            "residence": widget.residence,
+            "newBirthDate": widget.newBirthDate,
+            "email": emailController.text.trim(),
+            "createdAt": Timestamp.now(),
+          });
+
+          Navigator.pop(context);
         } else {
           // pop the loading circle
           //Navigator.pop(context);
           // show error message, passwords don't match
-          showErrorMessage("Passwords don't match!");
+          showErrorMessage(context, "Passwords don't match!");
         }
         // pop the loading circle
         Navigator.pop(context);
@@ -54,38 +81,9 @@ class _RegisterPageState extends State<RegisterPage> {
         // pop the loading circle
         Navigator.pop(context);
         // show error message
-        showErrorMessage(e.code);
+        showErrorMessage(context, e.code);
       }
     }
-
-    // error message to user
-    void showErrorMessage(String message) {
-      if (!mounted) return; // Vérifie si le widget est toujours actif
-
-      ScaffoldMessenger.of(context).clearSnackBars(); // Supprime les anciens SnackBars avant d'afficher un nouveau
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Container(
-            padding: const EdgeInsets.symmetric(vertical: 16), // Ajuste la hauteur ici
-            child: Text(
-              message,
-              style: const TextStyle(color: Colors.white, fontSize: 24),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          backgroundColor: Colors.red,
-          behavior: SnackBarBehavior.floating,
-          margin: const EdgeInsets.all(16), // Ajoute un peu d'espace autour
-          elevation: 6, // Ajoute une ombre pour un meilleur effet
-          duration: const Duration(seconds: 3),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20), // Arrondit les bords
-          ),
-        ),
-      );
-    }
-
-
 
     @override
     Widget build(BuildContext context) {
@@ -189,7 +187,22 @@ class _RegisterPageState extends State<RegisterPage> {
                                   children: [
                                       // google button
                                       SquareTile(
-                                        onTap: () => AuthService().signInWithGoogle(),
+                                        onTap: () async {
+                                          UserCredential? userCredential = await AuthService().signInWithGoogle(
+                                            firstName: widget.firstName,
+                                            lastName: widget.lastName,
+                                            residence: widget.residence,
+                                            newBirthDate: widget.newBirthDate,
+                                          );
+
+                                          // Si l'inscription est réussie, rediriger vers la HomePage
+                                          if (userCredential != null) {
+                                            Navigator.pushReplacement(
+                                              context,
+                                              MaterialPageRoute(builder: (context) => HomePage()),
+                                            );
+                                          }
+                                        },
                                         imagePath: 'lib/images/google.png',
                                       ),
                                       
