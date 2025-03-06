06be3adc697557adf5d02c5f7b42b9f005606edc
DanHaziel <danhazielnm@gmail.com>
2025-03-06 13:37:44 +0100

    outsourcing error management

---
+++ lib/errors/error_utils.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void showErrorMessage(BuildContext context, String message) {
+  ScaffoldMessenger.of(context).clearSnackBars(); // Supprime les anciens SnackBars avant d'afficher un nouveau
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        message,
+        style: const TextStyle(color: Colors.white),
+        textAlign: TextAlign.center,
+      ),
+      backgroundColor: Colors.red,
+      behavior: SnackBarBehavior.floating,
+      margin: const EdgeInsets.all(25), // Ajoute un peu d'espace autour
+      elevation: 6, // Ajoute une ombre pour un meilleur effet
+      duration: const Duration(seconds: 3),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20), // Arrondit les bords
+      ),
+    ),
+  );
+}
