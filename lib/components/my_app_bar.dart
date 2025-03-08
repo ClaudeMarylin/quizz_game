f87c82d4f1da746db639b4e425865fdb7a51a0d1
DanHaziel <danhazielnm@gmail.com>
2025-03-08 13:29:06 +0100

    Outsourcing the appBar to make it reusable

---
+++ lib/components/my_app_bar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String text;
+
+  const MyAppBar({
+    super.key,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.red[300],
+      centerTitle: true, // Centre le titre sur Android
+      title: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 16,
+        ),
+      ),
+      leading: IconButton(
+        icon: const Icon(Icons.menu, color: Colors.white),
+        onPressed: () {
+          Scaffold.of(context).openDrawer(); // Ouvre le Drawer
+        },
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
