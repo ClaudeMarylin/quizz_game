2c62d86430717095eac62c770ecfa8cb6e576657
DanHaziel <danhazielnm@gmail.com>
2025-03-11 17:32:36 +0100

    Adding the function to log out and rdefining the listTile

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:quizz_game/theme/theme_provider.dart';
+import 'package:quizz_game/components/my_list_tile.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
 
+  void signUserOut(BuildContext context) {
+    FirebaseAuth.instance.signOut().then((_) {
+      Navigator.pushReplacementNamed(context, "/login");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    final isDarkMode = themeProvider.themeData.brightness == Brightness.dark;
+
     return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.secondary,
       child: ListView(
-        padding: EdgeInsets.zero, // Enlève le padding par défaut
+        padding: EdgeInsets.zero,
         children: [
-          // Ajoute un Header dans le Drawer
           DrawerHeader(
             decoration: BoxDecoration(
-              color: Colors.red[300], // Utilisation de red[300]
+              color: Theme.of(context).colorScheme.tertiary,
             ),
             child: const SizedBox(
-              height: 100, // Remplace cette valeur par la hauteur souhaitée
+              height: 100,
               child: Text(
                 'Menu',
                 style: TextStyle(
@@ -25,84 +38,33 @@ class MyDrawer extends StatelessWidget {
               ),
             ),
           ),
-          ListTile(
-            leading: Icon(Icons.home), 
-            title: Text('Accueil')
-          ),
-          ListTile(
-            leading: Icon(Icons.person), 
-            title: Text('Profil')
-          ),
-          // Exemple d'item dans le Drawer
-          ListTile(
-            title: const Text('Page 1'),
+          MyListTile(
+            icon: const Icon(Icons.home),
+            text: "Accueil",
             onTap: () {
-              // Rediriger vers une page
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const Page1()),
-              );
+              Navigator.pushReplacementNamed(context, '/home');
             },
           ),
-          ListTile(
-            title: const Text('Page 2'),
+          MyListTile(
+            icon: const Icon(Icons.person),
+            text: "Profil",
+            onTap: () {},
+          ),
+          MyListTile(
+            icon: Icon(isDarkMode ? Icons.light_mode : Icons.dark_mode),
+            text: isDarkMode ? 'Mode clair' : 'Mode sombre',
             onTap: () {
-              // Rediriger vers une autre page
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const Page2()),
-              );
+              themeProvider.toggleTheme();
             },
           ),
-          ListTile(
-            title: const Text('Page 3'),
-            onTap: () {
-              // Rediriger vers une autre page
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const Page2()),
-              );
-            },
+          MyListTile(
+            icon: const Icon(Icons.logout, color: Colors.red),
+            text: "Déconnexion",
+            onTap: () => signUserOut(context), // Passer le contexte ici
           ),
-          // Ajouter d'autres éléments comme tu le souhaites
-
-          const Spacer(), // Pousse l'élément suivant en bas
-
-          ListTile(
-            leading: const Icon(Icons.logout, color: Colors.red),
-            title: const Text('Déconnexion'),
-            onTap: () {
-              // Ajoute ici l'action de déconnexion
-            },
-          ),
-
           const SizedBox(height: 10),
         ],
       ),
-    );
-  }
-}
-
-class Page1 extends StatelessWidget {
-  const Page1({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Page 1')),
-      body: const Center(child: Text('Contenu de la Page 1')),
-    );
-  }
-}
-
-class Page2 extends StatelessWidget {
-  const Page2({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Page 2')),
-      body: const Center(child: Text('Contenu de la Page 2')),
     );
   }
 }
